b5e504388807f2006d7ee163bfd5cfa148834b3c
Ahmed Aly <54458069+ahmealy@users.noreply.github.com>
2020-03-22 22:26:59 +0200

    Add files via upload

---
+++ Algorithms on Graphs/week1 Decomposition of Graphs 1/2_connected_components/connected_components.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::pair;
+
+void dfs(vector<vector<int> > &adj, int v, vector<bool> &visited)
+{
+    visited[v] = true;
+
+    for(int i = 0; i < adj[v].size(); i++)
+    {
+        if(!visited[adj[v][i]])  dfs(adj, adj[v][i], visited);
+    }
+}
+
+int number_of_components(vector<vector<int> > &adj) {
+  int res = 0;
+  //write your code here
+  vector<bool> visited(adj.size(),false);
+  for(int i = 0; i < adj.size(); i++)
+  {
+      if(!visited[i])
+      {
+          dfs(adj, i, visited);
+          res++;
+      }
+  }
+
+  return res;
+}
+
+int main() {
+  size_t n, m;
+  std::cin >> n >> m;
+  vector<vector<int> > adj(n, vector<int>());
+  for (size_t i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x - 1].push_back(y - 1);
+    adj[y - 1].push_back(x - 1);
+  }
+  std::cout << number_of_components(adj);
+}
